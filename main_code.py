d2d586a4bb2883db2fb0e7a46caf397fb23abdb3
Yogesh Gheu <65709105+YogeshGheu@users.noreply.github.com>
2020-08-11 17:48:25 +0530

    Update main_code.py

--- main_code.py
+++ main_code.py
@@ -9,13 +9,11 @@ def speaker(text):
 
 try: #for creating data.txt filr if dosen't exists. this will contain all the top 10 news of the day.
        with open("data.txt", "x") as createfile:
-              createfile.close()
               pass
 except FileExistsError:
        pass
 with open("data.txt", "w") as erase: #to erase the file if you start the program again/ or on next day
        erase.write("")
-       erase.close()
 
 url = ('https://newsapi.org/v2/top-headlines?country=in&apiKey=29d4f69e0b7e44eaa01d62e6b6545394') #You can change the country as per your requirements
 response = requests.get(url)
@@ -27,4 +25,3 @@ for i in range(0, 10):
               f.write(jsn['articles'][i]['title'])
               f.write("\n")
               sleep(1.5)
-f.close()
